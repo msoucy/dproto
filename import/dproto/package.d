4a436304f6e6b67f29672de410193df28a54c376
Matt Soucy <msoucy@csh.rit.edu>
2013-11-18 20:06:21 -0500

    Added package.d for 2.064 support
    Fixes #14

---
+++ import/dproto/package.d
@@ -0,0 +1,11 @@
+/*******************************************************************************
+ * Package import for dproto
+ *
+ * Imports the required components for the structure
+ *
+ * Authors: Matthew Soucy, msoucy@csh.rit.edu
+ * Date: Nov 18, 2013
+ * Version: 1.1.0
+ */
+module dproto;
+public import dproto.dproto;
