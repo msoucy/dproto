875fd05ee44cc57ec624ce9a1b1288c76c72af9b
Bjarne Bruhn <bjarne.bruhn@arcor.de>
2013-11-24 14:30:30 +0100

    enum standard conform

--- import/dproto/intermediate.d
+++ import/dproto/intermediate.d
@@ -270,7 +270,13 @@ struct Field {
 		}
 		if(requirement == Requirement.OPTIONAL) {
 			if(auto dV = "default" in options) {
-				ret ~= ", "~(*dV);
+                string dVprefix;
+                if (!IsBuiltinType(type)) {
+                    //only enums
+                    dVprefix = type~".";
+                }
+
+    			ret ~= ", "~(dVprefix)~(*dV);
 			} else if (IsBuiltinType(type)) {
 				ret ~= `, (BuffType!"%s").init`.format(type);
             } else {
