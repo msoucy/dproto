d8d63904b8a0386e6f0bc3035fdb3ed2df110d78
Matt Soucy <msoucy@csh.rit.edu>
2015-03-31 23:15:48 -0400

    Revamped dprotoc tool
    - Proper argument, help support
    - Fixes #44
    
    This'll be very helpful when debugging in the future

--- dprotoc/src/app.d
+++ dprotoc/src/app.d
@@ -2,26 +2,48 @@ import std.range;
 import std.algorithm;
 import std.getopt;
 import std.stdio;
+import std.string;
 import dproto.parse;
+import dproto.intermediate;
 
-auto openFileComplex(string fn, string mode) {
-	if(fn == "-") {
-		return (mode=="r") ? stdin : stdout;
-	} else {
-		return File(fn, mode);
-	}
+auto openFileComplex(string fn, string mode)
+{
+    if (fn == "-")
+    {
+        return (mode == "r") ? stdin : stdout;
+    }
+    else
+    {
+        return File(fn, mode);
+    }
 }
 
 void main(string[] args)
 {
-	string infile = "-";
-	string outfile = "-";
-	getopt(args,
-			"in", &infile,
-			"out", &outfile);
-	auto inf = openFileComplex(infile, "r");
-	auto of = openFileComplex(outfile, "w");
-	of.write(ParseProtoSchema(infile, inf.byLine.join("").idup));
-	inf.close();
-	of.close();
+    string infile = "-";
+    string outfile = "-";
+    string fmt = "%d";
+    auto helpInformation = getopt(
+			args,
+			"out|o", "Output filename (default stdout)", &outfile,
+			"format|f", "Code generation format", &fmt,
+	);
+    if (helpInformation.helpWanted)
+    {
+        defaultGetoptPrinter(
+				"Protocol Buffer D generator",
+				helpInformation.options);
+		return;
+    }
+    ProtoPackage pack;
+	{
+		File inf;
+		if(args.length == 2) {
+			inf = openFileComplex(args[1], "r");
+		} else {
+			inf = openFileComplex(infile, "r");
+		}
+		pack = ParseProtoSchema(infile, inf.byLine.join("\n").idup);
+	}
+    openFileComplex(outfile, "w").write(fmt.format(pack));
 }
