eb5dd527b2c308131b7db8c77af905553c6f170c
Matt Soucy <msoucy@csh.rit.edu>
2017-04-21 21:06:42 -0400

    Use built-in exception mixins

--- import/dproto/exception.d
+++ import/dproto/exception.d
@@ -4,27 +4,21 @@
  * Authors: Matthew Soucy, dproto@msoucy.me
  */
 module dproto.exception;
-import std.exception;
+import std.exception : basicExceptionCtors;
 
 /// Basic exception, something went wrong with creating a buffer struct
 class DProtoException : Exception {
-	this(string msg, string file=__FILE__, size_t line=__LINE__, Throwable next=null) {
-		super(msg, file, line, next);
-	}
+	///
+	mixin basicExceptionCtors;
 }
 
 /// Proto file attempted to use a reserved word
 class DProtoReservedWordException : DProtoException {
-	this(string word, string file=__FILE__, size_t line=__LINE__, Throwable next=null) {
-		super("Reserved word: "~word, file, line, next);
-		keyword = word;
-	}
-	string keyword;
+	///
+	mixin basicExceptionCtors;
 }
 
 /// Proto file used invalid syntax
 class DProtoSyntaxException : DProtoException {
-	this(string msg, string file=__FILE__, size_t line=__LINE__, Throwable next=null) {
-		super(msg, file, line, next);
-	}
+	mixin basicExceptionCtors;
 }
