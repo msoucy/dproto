aad519e530e997dbefb30d0b82612b44ea81e2d0
Kai Nacke <kai@redstar.de>
2017-08-18 21:09:48 +0200

    Fix Travis failure caused by use of new Phobos feature and old compiler.
    The mixin template basicExceptionCtors is a somewhat recent addition.

--- import/dproto/exception.d
+++ import/dproto/exception.d
@@ -4,21 +4,58 @@
  * Authors: Matthew Soucy, dproto@msoucy.me
  */
 module dproto.exception;
-import std.exception : basicExceptionCtors;
+import std.exception;
 
 /// Basic exception, something went wrong with creating a buffer struct
 class DProtoException : Exception {
-	///
-	mixin basicExceptionCtors;
+	static if (__traits(compiles, {mixin basicExceptionCtors;})) {
+		///
+		mixin basicExceptionCtors;
+	} else {
+		this(string msg, string file = __FILE__, size_t line = __LINE__,
+			 Throwable next = null) @safe pure nothrow {
+			super(msg, file, line, next);
+		}
+
+		this(string msg, Throwable next, string file = __FILE__,
+			 size_t line = __LINE__) @safe pure nothrow {
+			super(msg, file, line, next);
+		}
+	}
 }
 
 /// Proto file attempted to use a reserved word
 class DProtoReservedWordException : DProtoException {
-	///
-	mixin basicExceptionCtors;
+	static if (__traits(compiles, {mixin basicExceptionCtors;})) {
+		///
+		mixin basicExceptionCtors;
+	} else {
+		this(string msg, string file = __FILE__, size_t line = __LINE__,
+			 Throwable next = null) @safe pure nothrow {
+			super(msg, file, line, next);
+		}
+
+		this(string msg, Throwable next, string file = __FILE__,
+			 size_t line = __LINE__) @safe pure nothrow {
+			super(msg, file, line, next);
+		}
+	}
 }
 
 /// Proto file used invalid syntax
 class DProtoSyntaxException : DProtoException {
-	mixin basicExceptionCtors;
+	static if (__traits(compiles, {mixin basicExceptionCtors;})) {
+		///
+		mixin basicExceptionCtors;
+	} else {
+		this(string msg, string file = __FILE__, size_t line = __LINE__,
+			 Throwable next = null) @safe pure nothrow {
+			super(msg, file, line, next);
+		}
+
+		this(string msg, Throwable next, string file = __FILE__,
+			 size_t line = __LINE__) @safe pure nothrow {
+			super(msg, file, line, next);
+		}
+	}
 }
